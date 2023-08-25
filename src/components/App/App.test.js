f18332ac4b1c1b98fb265e03d03978f15f11a932
Yunus Emre Uyanık <y.emreuyanik@gmail.com>
2023-08-25 23:08:50 +0300

    Jest tests have been added

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -5,6 +5,7 @@ import App from './App';
 
 describe('App Component', () => {
     it('should render the Timer component with all buttons and initial time 25:00', () => {
+      //Rendering the component and we get it by destructing the getByText function. This function allows us to select the component element containing a certain text.
       const { getByText } = render(<App />);
       
       //looking for a button with the text 'Start','Stop''Reset','25:00'.
