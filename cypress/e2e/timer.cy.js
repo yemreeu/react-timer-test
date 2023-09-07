ef1f58f65bb3359f10072a609212baf60a6f85df
Yunus Emre Uyanık <y.emreuyanik@gmail.com>
2023-09-07 16:36:02 +0300

    added beforeEach method and unnecessary wait methods replaced

--- cypress/e2e/timer.cy.js
+++ cypress/e2e/timer.cy.js
@@ -1,33 +1,30 @@
+import { wait } from "@testing-library/react";
+
 describe("Timer App", () => {
-  it("Should wait for 5 seconds after the page loads", () => {
+  beforeEach(() => {
     cy.visit("/");
-    cy.wait(5000);
+  });
+
+  it("Should display '25:00' when the page is loaded", () => {
+    cy.get('[data-testid="time-display"]').should("contain", "25:00");
   });
 
   it("Should not allow stopping the timer before it's started", () => {
-    cy.visit("/");
-
     cy.get("[data-testid=stop-button]").click();
-
     cy.get("[data-testid=time-display]").should("contain", "25:00");
   });
 
   it("Should not allow resetting the timer before it's started", () => {
-    cy.visit("/");
-
     cy.get("[data-testid=reset-button]").click();
-
     cy.get("[data-testid=time-display]").should("contain", "25:00");
   });
 
   it("Should start the timer and check the initial time is 25:00", () => {
-    cy.visit("/");
     cy.get("[data-testid=time-display]").should("have.text", "25:00");
     cy.get("[data-testid=start-button]").click();
   });
 
   it("Should stop the timer after 5 seconds and check the time is 24:55", () => {
-    cy.visit("/");
     cy.get("[data-testid=start-button]").click();
     cy.wait(5000);
     cy.get("[data-testid=stop-button]").click();
@@ -35,15 +32,14 @@ describe("Timer App", () => {
   });
 
   it("Should reset the timer and check the time is 25:00", () => {
-    cy.visit("/");
     cy.get("[data-testid=start-button]").click();
-    cy.wait(5000);
+    cy.clock();
+    cy.tick(5000);
     cy.get("[data-testid=reset-button]").click();
     cy.get("[data-testid=time-display]").should("have.text", "25:00");
   });
 
   it("Should not restart the timer when 'Start' button is clicked while the timer is already running", () => {
-    cy.visit("/");
     cy.get("[data-testid=start-button]").click();
     cy.wait(1000);
     cy.get("[data-testid=start-button]").click();
@@ -51,74 +47,51 @@ describe("Timer App", () => {
   });
 
   it("Should reset the timer when 'Reset' button is clicked after stopping the timer", () => {
-    cy.visit("/");
     cy.get("[data-testid=start-button]").click();
-
-    cy.wait(5000);
-
+    cy.clock();
+    cy.tick(5000);
     cy.get("[data-testid=stop-button]").click();
-
     cy.get("[data-testid=reset-button]").click();
-
     cy.get("[data-testid=time-display]").should("contain", "25:00");
   });
 
   it("Should not change the timer when 'Stop' button is clicked multiple times while the timer is running", () => {
-    cy.visit("/");
     cy.get("[data-testid=start-button]").click();
-
-    cy.wait(1000);
-
+    cy.clock();
+    cy.tick(1000);
     cy.get("[data-testid=stop-button]").click();
     cy.get("[data-testid=time-display]").should("not.contain", /25\s*:/);
-
-    cy.wait(1000);
-
+    cy.clock();
+    cy.tick(1000);
     cy.get("[data-testid=stop-button]").click();
     cy.get("[data-testid=time-display]").should("not.contain", /25\s*:/);
-
-    cy.wait(1000);
-
+    cy.clock();
+    cy.tick(1000);
     cy.get("[data-testid=stop-button]").click();
     cy.get("[data-testid=time-display]").should("not.contain", /25\s*:/);
   });
 
   it("Should not go negative after 25 minutes and 5 seconds", () => {
-    cy.visit("/");
-
     cy.clock();
-
     cy.get("[data-testid=start-button]").click();
-
     cy.tick(25 * 60 * 1000 + 5000);
-
     cy.get("[data-testid=time-display]").should("not.contain", /-\d+\s*:/);
-
     cy.clock().invoke("restore");
   });
 
   it("Should handle timer timeout and reach 0:00", () => {
-    cy.visit("/");
-
     cy.clock();
-
     cy.get("[data-testid=start-button]").click();
-
     cy.tick(60 * 60 * 1000); // 1 hour
-
     cy.get("[data-testid=time-display]").should("contain", "0:00");
-
     cy.clock().invoke("restore");
   });
 
   it("Should persist timer functionality after page reload", () => {
-    cy.visit("/");
     cy.get("[data-testid=start-button]").click();
-
-    cy.wait(2000);
-
+    cy.clock();
+    cy.tick(2000);
     cy.reload();
-
     cy.get("[data-testid=time-display]").should("not.contain", /25\s*:/);
   });
 });
